ca2c0526a9af8bba6f5fb65ab91f4560a79cc62e
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-12-18 17:46:41 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
-
 package main
-
 import (
 	"fmt"
 	"net/http"
 )
-
 func main() {
 	port := ":8080"
 	fmt.Println("Server working on port :8080")
 
-	// http.HandleFunc("/", HandleRoot)
+	http.HandleFunc("/", HandleRoot)
 	
 	err := http.ListenAndServe(port, nil) 
 	if err != nil {
 		fmt.Println("Something Wrong...")
 	}
 }
-
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			fmt.Println("GET method")
+		case "POST":
+			fmt.Println("POST method")
+		case "PUT":
+			fmt.Println("PUT method")
+		case "DELETE":
+			fmt.Println("Delete method")
+	}
+}
